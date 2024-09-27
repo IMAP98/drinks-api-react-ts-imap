b9308cd86d70c7bfb80855ee6387adfd98976c68
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-09-27 13:37:02 -0600

    feat(services,stores,types,utils): RecipeServices.ts, recipeSlice, useAppSlice, global types and recipes-schema were added to the project

---
+++ src/services/RecipeService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { CategoriesAPIResponseSchema } from "../utils/recipes-schema";
+
+export const getCategories = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    const { data } = await axios(url);
+    const result = CategoriesAPIResponseSchema.safeParse(data);
+
+    if (result.success) {
+        return result.data;
+    }
+    
+}
